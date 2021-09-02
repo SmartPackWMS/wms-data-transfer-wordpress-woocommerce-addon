5774bbe7d25b1f39909379b80a7dfb6e98c17b08
Paris Nakita Kejser <contact@parisnakitakejser.com>
2021-09-02 21:17:53 +0200

    First step to take Woo products out

--- src/Controllers/CLI/Product_Controller.php
+++ src/Controllers/CLI/Product_Controller.php
@@ -8,4 +8,32 @@ class CLI_Products
     {
         \WP_CLI::line('Hello World!');
     }
+
+    function execute()
+    {
+        $args = array(
+            'post_type'      => 'product',
+            'posts_per_page' => 10
+        );
+
+        $loop = new \WP_Query($args);
+
+        while ($loop->have_posts()) {
+            $loop->the_post();
+            global $product;
+
+            if ($product->get_sku()) {
+                $item = [
+                    'sku' => $product->get_sku(),
+                    'description' => get_the_title()
+                ];
+
+                print_r($item);
+            } else {
+                echo 'Product missing SKU number';
+            }
+        }
+
+        wp_reset_query();
+    }
 }
--- src/Controllers/CLI_Controller.php
+++ src/Controllers/CLI_Controller.php
@@ -13,7 +13,7 @@ class CLI
         add_action('cli_init', function () {
             $product_cli = new CLI_Products();
 
-            \WP_CLI::add_command('smartpack:product:sync', $product_cli->hello_world());
+            \WP_CLI::add_command('smartpack:product:sync', $product_cli->execute());
             \WP_CLI::add_command('smartpack:stock:sync', $product_cli->hello_world());
             \WP_CLI::add_command('smartpack:order:sync', $product_cli->hello_world());
         });
