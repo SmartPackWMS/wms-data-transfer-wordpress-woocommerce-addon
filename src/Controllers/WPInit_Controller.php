72447589e70ba452792d8f8502d52576a929ba44
Paris Nakita Kejser <contact@parisnakitakejser.com>
2021-09-04 15:49:11 +0200

    add action when order in Woo get changend

--- src/Controllers/WPInit_Controller.php
+++ src/Controllers/WPInit_Controller.php
@@ -8,9 +8,40 @@ class WPInit_Controller
 {
     function init()
     {
-        add_action('woocommerce_update_product', function ($product_id) {
-            update_post_meta($product_id, 'smartpack_wms_state', 'pending');
-            update_post_meta($product_id, 'smartpack_wms_changed', new DateTime());
+        add_action('woocommerce_update_product', function ($post_id) {
+            update_post_meta($post_id, 'smartpack_wms_state', 'pending');
+            update_post_meta($post_id, 'smartpack_wms_changed', new DateTime());
         }, 10, 1);
+
+        // Order Status Change
+        add_action('woocommerce_order_status_pending', function ($order_id) {
+            // pending
+        });
+        add_action('woocommerce_order_status_failed', function ($order_id) {
+            // failed
+        });
+
+        add_action('woocommerce_order_status_on-hold', function ($order_id) {
+            // on-hold
+        });
+
+        add_action('woocommerce_order_status_processing', function ($order_id) {
+            update_post_meta($order_id, 'smartpack_wms_state', 'pending');
+            update_post_meta($order_id, 'smartpack_wms_order_state', 'pending');
+            update_post_meta($order_id, 'smartpack_wc_order_state', 'processing');
+            update_post_meta($order_id, 'smartpack_wms_changed', new DateTime());
+        });
+
+        add_action('woocommerce_order_status_completed', function ($order_id) {
+            // completed
+        });
+
+        add_action('woocommerce_order_status_refunded', function ($order_id) {
+            // refunded
+        });
+
+        add_action('woocommerce_order_status_cancelled', function ($order_id) {
+            // cancelled
+        });
     }
 }
